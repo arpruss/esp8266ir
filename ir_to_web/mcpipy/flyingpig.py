6a8b35c6d749764b50975cc21ac0fb18e60734c4
arpruss <arpruss@gmail.com>
2016-06-28 13:30:31 -0500

    improve flying pig

--- ir_to_web/mcpipy/flyingpig.py
+++ ir_to_web/mcpipy/flyingpig.py
@@ -5,7 +5,7 @@ from sys import argv
 
 mc = Minecraft()
 
-if len(argv)>1 and argv[1] == 'player':
+if len(argv)>1 and argv[1] == 'me':
     x,y,z = mc.player.getPos()
     pig = mc.getPlayerId()
 else:
@@ -13,13 +13,35 @@ else:
     x,y,z = playerPos.x,playerPos.y,playerPos.z+1
     pig = mc.spawnEntity(PIG if len(argv)<=1 else argv[1], x,y,z, '{NoAI:1}')
 
-yaw = mc.entity.getRotation(pig)
 remote = ESPRemote()
 
+yaw = mc.entity.getRotation(pig)
+pitch = 0
+newY = y
+dyaw = 0
+
 lastTime = time()
+lastSignal = -1000
 while True:
-    dt = time()-lastTime
+    if time() - lastSignal > 1:
+        dyaw = 0
+        pitch = 0
+        newY = y
+    else:
+        dt = time()-lastTime
+        yaw -= dyaw * dt * 2.5
+        x += dt * .1 * pitch * -sin(radians(yaw))
+        z += dt * .1 * pitch * cos(radians(yaw))
+        if newY-y > 4 * dt:
+            y += 4 * dt
+        elif y-newY > 4 * dt:
+            y -= 4 * dt
+        else:
+            y = newY
     lastTime = time()
+    mc.entity.setRotation(pig, yaw)
+    mc.entity.setPitch(pig, pitch)
+    mc.entity.setPos(pig, x,y,z)
     if remote.available():
         pitch = 0
         dyaw = 0
@@ -29,13 +51,9 @@ while True:
             if extras:            
                 throttle = extras.get('throttle',0)
                 pitch = 63 - extras.get('pitch', 63)
-                dyaw = extras.get('yaw', 63) - 63 - (63 - extras.get('trim', 63))
-                y = throttle * 0.1
-        yaw -= dyaw * dt * 2.5
-        x += dt * .1 * pitch * -sin(radians(yaw))
-        z += dt * .1 * pitch * cos(radians(yaw))
-        mc.entity.setRotation(pig, yaw)
-        mc.entity.setPos(pig, x,y,z)
+                dyaw = extras.get('yaw', 63) + extras.get('trim', 63) - 2*63
+                newY = throttle * 0.4
+                lastSignal = time()
     else:
-        sleep(0.25)
+        sleep(0.2)
     
