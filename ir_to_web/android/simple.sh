425e72324f421880f9154a5f516da9777f417ad3
arpruss <arpruss@gmail.com>
2016-06-29 08:36:30 -0500

    foo

--- ir_to_web/android/simple.sh
+++ ir_to_web/android/simple.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 nc 192.168.1.123 5678 | while read line ; do
-  if [[ $line == *454? ]]
+  if [[ $line == *458? ]]
   then
     echo "Volume Up"
     input keyevent KEYCODE_VOLUME_UP
-  elif [[ $line == *421? ]]
+  elif [[ $line == *459? ]]
   then
     echo "Volume Down"
     input keyevent KEYCODE_VOLUME_DOWN
